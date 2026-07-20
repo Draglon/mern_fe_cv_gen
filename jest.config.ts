03b6955da28e47ec736b2c5edb8b6d05022eaec7
Draglon <s.nazarchuck@gmail.com>
2026-07-20 20:52:33 +0300

    [FE-29] remove cypress. Write tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,14 @@
 import type { Config } from "jest";
 import nextJest from "next/jest.js";
-
-const createJestConfig = nextJest({
-  dir: "./",
-});
+ 
+const createJestConfig = nextJest({ dir: './' });
 
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!**/node_modules/**',
-  ],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], 
 };
 
 export default createJestConfig(config);
--- src/views/shared/antd/Alert/__tests__/index.test.tsx
+++ src/views/shared/antd/Alert/__tests__/index.test.tsx
@@ -1,4 +1,3 @@
-import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 import { ALERT_KINDS } from "@/lib/constants/alert";
